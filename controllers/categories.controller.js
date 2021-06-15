27c033493e16fe26e7b0b461bb37e01d095cb6ac
ushan1997 <ushananuka5@gmail.com>
2021-06-15 11:23:41 +0530

    calculation function started

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -45,23 +45,28 @@ router.get('/getVehicalsfromId/:id',async (req,res)=>{
 router.get('/test/:id',async (req,res)=>{
      await Categories.findById(req.params.id)
     .then(data=>{
-        res.status(200).send({data: data.vehicles})})
-    .catch(err =>{
+        res.status(200).send({data: data.duration})
+    }).catch(err =>{
         res.status(500).send({err: err.message});
     })
 })
+router.get('/getduration/:id',async(req, res)=>{
+    await Categories.findById(req.params.id)
+    .then(data => res.status(200).send({data: data.duration}))
+    .catch(err => res.status(500).send({err: err.message}))
+})
 
 router.get('/:type/:duration' ,async (req,res)=>{
-    //if(req.params && /*req.params.vehicle &&**/ req.params.name && req.params.type && req.params.duration){
+    await Categories.find({type:req.params.type,duration:req.params.duration})
+    .then(data =>{
+        res.status(200).send({data: data.duration})
+    }).catch(err =>{
+        res.status(500).send({err: err.message})
+    })
 
-        await Categories.find({type:req.params.type,duration:req.params.duration})
-        .then(data =>{
-            res.status(200).send({data: data.duration})
-        }).catch(err =>{
-            res.status(500).send({err: err.message})
-        })
-   
 })
+
+
 
 
 module.exports=router;
